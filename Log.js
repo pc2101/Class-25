01ce11cf4665365ba54a2a3326714cacd1b84d98
pc2101 <74606397+pc2101@users.noreply.github.com>
2020-12-16 20:51:40 +0530

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,8 @@
+class Log extends ParentClass {
+  constructor (x,y,height,angle){
+    super (x,y,20,height, height)
+    this.image = loadImage ("sprites/wood2.png")
+    Matter.Body.setAngle(this.body,angle)
+  }
+
+}
